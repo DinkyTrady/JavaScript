90d00e7c871d19666fb940dc03bb905dfda08ac5
Randy <rnys673@gmail.com>
2022-10-24 10:27:31 +0700

    add new explanation

--- oop-javascript/constructor-function/constructor-function.js
+++ oop-javascript/constructor-function/constructor-function.js
@@ -1,12 +1,12 @@
 //This is the constructor function in javascript.
-//The name of function should start with Uppercas.
+//The name of function should start with Uppercase.
 //Like this :
 function Person() {
 
 }
 
-// This how to call constructor function.
+// This is how make constructor-function inside variable
 const person = new Person()
 
-// Call constructor function from where is inside variable
+// Call constructor function from the variable where is the inside of it is constructor-function
 console.log(person)
