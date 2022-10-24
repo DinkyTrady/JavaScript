94f86fefad23513a4a42317566f7ba12c6585562
Randy <rnys673@gmail.com>
2022-10-24 10:21:17 +0700

    add new line in `constructor-funtion.js`

--- oop-javascript/constructor-function/constructor-function.js
+++ oop-javascript/constructor-function/constructor-function.js
@@ -1,6 +1,12 @@
-//This is the function generator ib javascript
-//The name of function should start with Uppercase
+//This is the constructor function in javascript.
+//The name of function should start with Uppercas.
 //Like this :
 function Person() {
 
 }
+
+// This how to call constructor function.
+const person = new Person()
+
+// Call constructor function from where is inside variable
+console.log(person)
