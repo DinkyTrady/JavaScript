83e74c2d5d5bccef0cfd03543a0be8f5054f5567
Kyra <rnys673@gmail.com>
2022-11-06 19:36:19 +0700

    nambahin class

---
+++ oop-javascript/class/class.js
@@ -0,0 +1,10 @@
+// Jadi untuk class di JavaScript nenggunakan syntax seperti dibawah
+class name {
+
+}
+
+//Masukin kedalam variable agar bisa di panggil
+const nama = new name();
+
+//Panggil
+console.log(nama)
