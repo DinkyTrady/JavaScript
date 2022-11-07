e54cc733ae47f270ae2e746d05d35a420b5cb8a8
Kyra <rnys673@gmail.com>
2022-11-07 13:02:02 +0700

    adding new file

---
+++ oop-javascript/constructor-function/method-cf.js
@@ -0,0 +1,36 @@
+// Menggunakan method dalam constructor-function  
+
+function Nama(first, two) {
+  this.firstChild = first
+  this.secondChild = two
+  this.sayIt = function (three) {
+    return `hi ${this.firstChild} and ${this.secondChild}, i'm ${three}`
+  }
+}
+
+// Pertama - tama mari kita masukin constructor-function kedalam variable object seperti biasa
+const nama = new Nama('budi', 'roger');
+
+// jika kita ingin memanggil method di dalam constructor-function kita bisa menggunakan cara seperti ini
+console.log(nama.sayIt('your Dad'))
+
+
+{
+//contoh tak menggunakan kata kunci return
+  function Nama(first, two) {
+    this.firstChild = first
+    this.secondChild = two
+    this.sayIt = function (three) {
+      return `hi ${this.firstChild} and ${this.secondChild}, i'm ${three}`
+    }
+    this.sayHello = function (say) {
+      console.log(`hi i'm ${say}`)
+    }
+  }
+
+  // masukin constructor-function kedalam variable object seperti biasa
+  const nama = new Nama('budi', 'roger');
+
+  // panggil method yang menggunakan console.log
+  nama.sayHello('kanibal')
+}
---
+++ oop-javascript/constructor-function/property-cf.js
@@ -0,0 +1,26 @@
+// Cara membuat property di constructor-function sangatlah mudah kita bisa menggunakan kata kunci (this)
+// Contoh :
+function Nama() {
+  this.firstName = 'Budi'
+  this.middleName = 'Roger'
+}
+
+// buat object dengan isi constructor-function yang telah kita buat
+const name = new Nama();
+
+// panggil dengan console.log
+console.log(name)
+
+
+
+//menggunakan property dengan parameter
+function Name(first, mid) {
+  this.firstName = first
+  this.middleName = mid
+}
+
+//Masukan parameter disaat memasukan constructor-function kedalam sebuah object
+const nama = new Name('Budi', 'Saputro');
+
+//Panggil objectnya
+console.log(nama)
