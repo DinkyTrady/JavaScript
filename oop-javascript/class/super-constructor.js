49fbd3debed5d797fb755b1d8a941be88c472f83
DinkyTrady <DinkyTrady@kyra.com>
2024-03-02 16:20:42 +0700

    memperbaiki comment

--- oop-javascript/class/super-constructor.js
+++ oop-javascript/class/super-constructor.js
@@ -9,12 +9,15 @@ const nama = new Name("Ghost");
 console.log(nama);
 
 class Other extends Name {
-  // untuk memanggunakan kata kunci super kita perlu menyamakan parameter yang ada di class yang kita extends jadi kita tak perlu lagi menggunakan kata kunci this di dalam constructor dikarenakan parents dari class ini sudah ada deklarasi this
-  constructor(nama) {
+  // untuk memanggunakan kata kunci super kita perlu menyamakan parameter
+  // yang ada di class yang kita extends jadi kita tak perlu lagi menggunakan
+  // kata kunci this di dalam constructor dikarenakan parents dari class ini sudah ada deklarasi this
+  constructor(nama, age) {
     super(nama);
+    this.age = age;
   }
 }
 
-const other = new Other("Animals");
+const other = new Other("Animals", 15);
 
 console.log(other);
